f5cae809137e8ca4b842df6dd957b60f59fb3e45
Patrick Gostovic <pg@phranq.com>
2013-09-27 10:07:13 -0400

    Heroku is in charge of the port.

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var configBool = function(name, defaultValue)
 module.exports =
 {
     host: config("HOST", os.hostname()),
-    port: configNum("PORT", 8888),
+    port: configNum("PORT", process.env.PORT),
     secure: configBool("SECURE", false),
     dbConnStr: config("DB_CONN_STR", "mongodb://localhost/pilot_dev"),
     passwordSalt: config("PASSWORD_SALT", "abc123")
