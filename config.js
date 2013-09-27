858bbb84d34ec8b148f3b9d83d9e122c0cdd00a5
Patrick Gostovic <pg@phranq.com>
2013-09-27 08:51:11 -0400

    Default port 8888

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var configBool = function(name, defaultValue)
 module.exports =
 {
     host: config("HOST", os.hostname()),
-    port: configNum("PORT", 7777),
+    port: configNum("PORT", 8888),
     secure: configBool("SECURE", false),
     dbConnStr: config("DB_CONN_STR", "mongodb://localhost/pilot_dev"),
     passwordSalt: config("PASSWORD_SALT", "abc123")
